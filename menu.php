286f7ac025f988caa5902d994f33766289c9da07
dmyr0613 <dmyr0613@gmail.com>
2022-04-06 18:07:56 +0900

    1

--- menu.php
+++ menu.php
@@ -27,6 +27,11 @@
 									</ul>
 								</nav>
 
+								<!-- Footer -->
+									<footer id="footer">
+										<p class="copyright">&copy; SBS Information Systems Co.,Ltd. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+									</footer>
+									
 						</div>
 					</div>
 

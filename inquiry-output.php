0706132a88ae49f0dcfb3f7ea3c3e9c92fa89230
dmyr0613 <dmyr0613@gmail.com>
2022-04-06 17:54:23 +0900

    1

--- inquiry-output.php
+++ inquiry-output.php
@@ -23,7 +23,7 @@
 						}
 
 						if (isset($_SESSION['inquiry'])) {
-							// お問合せセッション情報ある場合は、inquiryテーブルをUPDATE。
+							// お問合せセッション情報がある場合は、inquiryテーブルをUPDATE
 							$sql=$pdo->prepare('update inquiry set priority_flg=?, order_kind=?, contents=?, kanja_id=?, sbs_comment=? where inquiry_no=?');
 							$sql->execute([
 								0,
@@ -35,10 +35,10 @@
 
 								// お問合せセッション情報を更新
 								$_SESSION['inquiry']=[
-									// 'inquiry_no'=>1,
-									// 'user_id'=>$user_id,
-									// 'facility_code'=>$facility_code,
-									// 'facility_name'=>$facility_name,
+									'inquiry_no'=>$_SESSION['inquiry']['inquiry_no'],
+									'user_id'=>$user_id,							//念のため再設定
+									'facility_code'=>$facility_code,	//念のため再設定
+									'facility_name'=>$facility_name,	//念のため再設定
 									'priority_flg'=>0,
 									'order_kind'=>$_REQUEST['order_kind'],
 									'contents'=>$_REQUEST['contents'],
@@ -47,10 +47,6 @@
 
 							echo '<p>お問合せ情報を更新しました。<br>';
 							echo 'サポートセンターからの回答をお待ちください。</p>';
-							echo '<ul class="actions">';
-							echo '<li><a href="inquiry.php" class="button big">お問合せフォームに戻る</a></li>';
-							echo '<li><a href="main.php" class="button big">ホーム</a></li>';
-							echo '</ul>';
 
 						} else {
 							// 新規お問合せ登録
@@ -65,7 +61,7 @@
 								$_REQUEST['kanja_id'],
 								$_REQUEST['sbs_comment']]);
 
-							// 登録したinquiry_noを取得
+							// 今登録したinquiry_noを取得
 							$inquiry_no=0;
 							$sql=$pdo->prepare('select currval(\'inquiry_seq\')');
 							$sql->execute();
@@ -87,11 +83,11 @@
 
 							echo '<p>お問合せ情報を登録しました。<br>';
 							echo 'サポートセンターからの回答をお待ちください。</p>';
-							echo '<ul class="actions">';
-							echo '<li><a href="inquiry.php" class="button big">お問合せフォームに戻る</a></li>';
-							echo '<li><a href="main.php" class="button big">ホーム</a></li>';
-							echo '</ul>';
 						}
+						echo '<ul class="actions">';
+						echo '<li><a href="inquiry.php" class="button big">お問合せフォームに戻る</a></li>';
+						echo '<li><a href="main.php" class="button big">ホーム</a></li>';
+						echo '</ul>';
 					?>
 				</section>
 

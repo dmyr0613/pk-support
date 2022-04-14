ba5faf3ce744687c442c2326a3abe587af405575
dmyr0613 <dmyr0613@gmail.com>
2022-04-14 17:47:46 +0900

    1

--- iconsearch-output.php
+++ iconsearch-output.php
@@ -14,36 +14,23 @@
 						<tbody>
 
 						<?php
-						  // error_log('select * from iconlist where keyword like \'%?%\' order by code');
-							// error_log($_REQUEST['search']);
-							// error_log("select * from iconlist where keyword like \'%" , $_REQUEST['search'] , "%\' order by code");
 
+							echo '<h2 id="content">' , $_POST['search'], 'の検索結果</h2>';
+							echo '<p>アイコン素材はフリーで商用利用可能です。</p>';
+							echo '<p>右クリックメニューの「名前を付けて画像を保存...」からダウンロードしてください。</p>';
 
 							//アイコン検索
-							// $sql=$pdo->prepare('select * from iconlist where keyword like \'%?%\' order by code');
-							// $sql->execute($_REQUEST['search']);
-
 							$sqltxt  = 'select * from iconlist where keyword like \'%';
 							$sqltxt .= $_POST['search'];
 							$sqltxt .= '%\' order by code';
-							error_log($sqltxt);
 							$sql = $pdo->prepare($sqltxt);
 							$sql->execute();
 
-
-							// $sqltxt = 'select * from iconlist where keyword like \'%:search%\' order by code';
-						  // $sql = $pdo->prepare($sqltxt);
-						  // $sql->bindValue(':search', $_REQUEST['search']);
-						  // $sql->execute();
-
-							// $sql=$pdo->prepare('select * from iconlist where keyword like \'%医師%\' order by code');
-							// $sql->execute();
-
-							// $sql->execute($_REQUEST['search']);
 							foreach ($sql as $row) {
-
+								//検索結果を表示
 								echo '<tr>';
-								echo '	<td><p><img src="icon/512w/' , $row['icon_name'] , '.png" width="200"></p></td>';
+								echo '	<td><img src="icon/512w/' , $row['icon_name'] , '.png" width="200"></td>';
+								echo '	<td>', $row['disp_name'], '</td>';
 								echo '</tr>';
 							}
 						?>

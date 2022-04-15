a480cabc468db255f31ed70e54adf107a1768436
dmyr0613 <dmyr0613@gmail.com>
2022-04-15 11:17:29 +0900

    1

--- test_mail.php
+++ test_mail.php
@@ -4,15 +4,15 @@
   </head>
   <body>
   <?php
-    // mb_language("Japanese");
-    // mb_internal_encoding("UTF-8");
+    mb_language("Japanese");
+    mb_internal_encoding("UTF-8");
 
     $to = $_POST['to'];
     $title = $_POST['title'];
     $message = $_POST['message'];
     $headers = "From: from@example.com";
 
-    if(mb_send_mail($to, $title, $message, headers))
+    if(mb_send_mail($to, $title, $message, $headers))
     {
       echo "メール送信成功です";
     }

19a84db060f7ccc1faff1b15d5dd0ce2e91adb64
dmyr0613 <dmyr0613@gmail.com>
2022-04-15 19:16:40 +0900

    1

--- status-list.php
+++ status-list.php
@@ -43,13 +43,18 @@
 								<?php
 								//現在時刻を取得
 								date_default_timezone_set('Asia/Tokyo');
-								error_log("from_date : " . date("Y-m-d", strtotime("Y-m-d -1 month")));
-								error_log("to_date : " . date("Y-m-d"));
-								echo date("Y-m-d", strtotime("2021-09-30 -1 day")); //2021-09-29
-
-								echo '<input type="date" name="from_date" value="', date("Y-m-d", strtotime("YYYY-mm-dd -1 month")) ,'">';
+								// error_log("from_date : " . date("Y-m-d", strtotime("Y-m-d -1 month")));
+								// error_log("to_date : " . date("Y-m-d"));
+								// echo date("Y-m-d", strtotime("2021-09-30 -1 day")); //2021-09-29
+								$from_date = date("Y/m/d");
+								$from_date->modify('-1 months');
+								$to_date = date("Y/m/d");
+								error_log("from_date : " . $from_date);
+								error_log("to_date : " . $to_date);
+								
+								echo '<input type="date" name="from_date" value="', $from_date ,'">';
 								echo ' 〜 ';
-								echo '<input type="date" name="to_date" value="', date("Y-m-d") ,'">';
+								echo '<input type="date" name="to_date" value="', $to_date ,'">';
 								?>
 							</p>
 							<input type="submit" value="再検索">
